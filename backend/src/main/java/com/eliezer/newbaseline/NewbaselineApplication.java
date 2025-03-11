79e9f8ffb49fc5a602d7a4bbb590cbb04d50d9f5
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2025-03-11 15:01:39 -0300

    :card_file_box: raw: Added RabbitMQ dependency

--- backend/src/main/java/com/eliezer/newbaseline/NewbaselineApplication.java
+++ backend/src/main/java/com/eliezer/newbaseline/NewbaselineApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewbaselineApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NewbaselineApplication.class, args);
 	}
-
 }
