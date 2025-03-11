0978a16f577e33a1c3457ea6ff0128bd61f5f7fe
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2025-03-11 11:57:15 -0300

    :rocket: feat: First commit. Created Spring Initializer project

---
+++ backend/src/main/java/com/eliezer/newbaseline/NewbaselineApplication.java
@@ -0,0 +1,13 @@
+package com.eliezer.newbaseline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbaselineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewbaselineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eliezer/newbaseline/NewbaselineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliezer.newbaseline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbaselineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
