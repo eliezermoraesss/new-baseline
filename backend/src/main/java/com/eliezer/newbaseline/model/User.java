81665bab074f06e4e0a7353aa9a7151fc5c7f2f7
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2025-03-13 08:29:19 -0300

    feat: Implementado classe model User

--- backend/src/main/java/com/eliezer/newbaseline/model/User.java
+++ backend/src/main/java/com/eliezer/newbaseline/model/User.java
@@ -1,4 +1,39 @@
 package com.eliezer.newbaseline.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(columnDefinition = "TEXT")
+    private String profilePicture;
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
 }
