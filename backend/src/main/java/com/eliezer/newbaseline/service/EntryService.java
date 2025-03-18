cbd033fd9dd193f76a8c35ffde004501cb32d0f4
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2025-03-18 09:25:44 -0300

    :construction: Implementado classe EntryService

--- backend/src/main/java/com/eliezer/newbaseline/service/EntryService.java
+++ backend/src/main/java/com/eliezer/newbaseline/service/EntryService.java
@@ -1,4 +1,64 @@
 package com.eliezer.newbaseline.service;
 
+import com.eliezer.newbaseline.dto.request.EntryRequestDTO;
+import com.eliezer.newbaseline.dto.response.EntryResponseDTO;
+import com.eliezer.newbaseline.mapper.EntryMapper;
+import com.eliezer.newbaseline.model.Entry;
+import com.eliezer.newbaseline.repository.postgres.EntryRepository;
+import com.eliezer.newbaseline.service.exception.DataBaseException;
+import com.eliezer.newbaseline.service.exception.ResourceNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class EntryService {
+    public static final String MSG_NOT_FOUND = "Entry Id not found: ";
+    private final EntryRepository entryRepository;
+    private final EntryMapper entryMapper;
+
+    public EntryService(EntryRepository entryRepository, EntryMapper entryMapper) {
+        this.entryRepository = entryRepository;
+        this.entryMapper = entryMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public List<EntryResponseDTO> findAll() {
+        return entryRepository.findAll().stream().map(entryMapper::toDTO).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public EntryResponseDTO findById(Long id) {
+        return entryMapper.toDTO(entryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(MSG_NOT_FOUND + id)));
+    }
+
+    @Transactional
+    public EntryResponseDTO insert(EntryRequestDTO entryRequestDTO) {
+        Entry entry = entryMapper.toEntity(entryRequestDTO);
+        entry = entryRepository.save(entry);
+        return entryMapper.toDTO(entry);
+    }
+
+    @Transactional
+    public EntryResponseDTO update(Long id, EntryRequestDTO entryRequestDTO) {
+        Entry entry = entryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(MSG_NOT_FOUND + id));
+        entryMapper.updateEntryFromDTO(entryRequestDTO, entry);
+        entry = entryRepository.save(entry);
+        return entryMapper.toDTO(entry);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void delete(Long id) {
+        try {
+            entryRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(MSG_NOT_FOUND + id + " - " + e.getMessage());
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Integrity violation - " + e.getMessage());
+        }
+    }
 }
